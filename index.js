d42ab072c6d161366bd3bb818739b39adbbefed1
Hamna Kamran <74135890+hamnakamran10@users.noreply.github.com>
2021-05-18 08:59:34 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+let x = 0;
+
+
+  while(x<10){
+
+      x++;
+      console.log(x)
+
+
+  }
+
+  let num = prompt("enter");
+
+  for(let i=1; i<11 ; i++){
+    result= num*i;
+
+   console.log(result)
+  }
+//Data types
+let numbers=12;
+let string="Unais";
+let object={firstname:"unais ", lastname:"ali", hobbies:"cricket" ,age:17}
+let float= 12.3;
+let boolean= true;
+let array=["unais","umais","mango"]
+let random;
+let nothing=null;
+//String 
+let sport="Cricket";
+let moresports="hockey\nfootball"
+let test="Pakistan commemorating"
+console.log(sport.length)
+console.log(moresports.length)
+console.log(moresports.indexOf(""))
+console.log(test.slice(17,19));
+console.log(moresports.replace("hockey","tenis"))
